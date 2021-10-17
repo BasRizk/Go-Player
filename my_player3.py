40590268b95bbadc22c5db715c6062be80c3b25f
basem.rizk@outlook.com <basem.rizk@outlook.com>
2021-10-17 16:07:17 +0200

    fixxed mapping of identical states dicts generation + some debugging methods and commented statements added

--- my_player3.py
+++ my_player3.py
@@ -15,12 +15,12 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 
 
 WIN_REWARD = 1
-DRAW_REWARD = 0
-LOSS_REWARD = -1
+DRAW_REWARD = 0.5
+LOSS_REWARD = 0
     
 class QValues:
 
-    def __init__(self, N, initial_value):
+    def __init__(self, N, initial_value, verbose=0):
         self.N = N
         self.board_size = N*N
         self.q_tables = {}
@@ -29,6 +29,8 @@ class QValues:
         self.recent_board_state = None
         self.recent_state_q_values = None
         self.index_of_encoding = None
+        self.recent_board_encoding = None
+        self.verbose = verbose
 
 
     def init_mappings(self):
@@ -45,27 +47,91 @@ class QValues:
         
         board_sample = np.arange(0, self.board_size).reshape((self.N,self.N))
         equi_boards = [
-                board_sample,
-                np.rot90(board_sample, k=1, axes=(1,0)),    # rotate 90 right
-                np.rot90(board_sample, k=1, axes=(0,1)),    # rotate 90 left
-                np.rot90(board_sample, k=2, axes=(1,0)),    # rotate 180 right
-                board_sample.T,                             # transpose
-                np.rot90(board_sample, k=1, axes=(0,1)).T,  # flip over y-axis
-                np.rot90(board_sample, k=1, axes=(1,0)).T,  # flip over x-axis
-                np.rot90(board_sample, k=2, axes=(1,0)).T   # flip 180 rotation
+                deepcopy(board_sample),
+                np.rot90(deepcopy(board_sample), k=1, axes=(1,0)),    # rotate 90 right
+                np.rot90(deepcopy(board_sample), k=1, axes=(0,1)),    # rotate 90 left
+                np.rot90(deepcopy(board_sample), k=2, axes=(1,0)),    # rotate 180 right
+                deepcopy(board_sample).T,                             # transpose
+                np.rot90(deepcopy(board_sample), k=1, axes=(0,1)).T,  # flip over y-axis
+                np.rot90(deepcopy(board_sample), k=1, axes=(1,0)).T,  # flip over x-axis
+                np.rot90(deepcopy(board_sample), k=2, axes=(1,0)).T   # flip 180 rotation
             ]
         
         self.sym_dict_forward = [{} for i in range(len(equi_boards))]
         self.sym_dict_backward = [{} for i in range(len(equi_boards))]
-                         
+            
         for d_i in range(len(equi_boards)):
             for i in range(self.N):
                 for j in range(self.N):
-                    self.sym_dict_forward[d_i][board_sample[i][j]] =\
-                        equi_boards[d_i][i][j]
-                    self.sym_dict_backward[d_i][equi_boards[d_i][i][j]] =\
-                        board_sample[i][j]
+                    c = board_sample[i][j]
+                    x, y = np.asarray(np.where(c == equi_boards[d_i]))
+                    x, y = x[0], y[0]
+                    self.sym_dict_backward[d_i][c] = board_sample[x][y]
+                        # equi_boards[d_i][i][j]
+                    # self.sym_dict_forward[d_i][equi_boards[d_i][i][j]] = c
+                    self.sym_dict_forward[d_i][c] = equi_boards[d_i][x][y]
+    
+        # for d_i in range(len(equi_boards)):
+        #     for i in range(self.N):
+        #         for j in range(self.N):
+        #             self.sym_dict_backward[d_i][board_sample[i][j]] =\
+        #                 equi_boards[d_i][i][j]
+        #             self.sym_dict_forward[d_i][equi_boards[d_i][i][j]] =\
+        #                 board_sample[i][j]
 
+    def visualize_q_table(self, board_state, encoding_inc=None, verbose=0):
+        def fit_table(table):
+            fit_table = np.zeros(len(table) - 1)
+            for i in range(len(fit_table)):
+                fit_table[i] =\
+                    table[self.sym_dict_backward[self.index_of_encoding][i]]
+                if self.verbose > 2:
+                    print('fit_table at %d moved to %d with value=%.2f' 
+                          % (i, self.sym_dict_backward[self.index_of_encoding][i],
+                          table[self.sym_dict_backward[self.index_of_encoding][i]]))
+            return fit_table
+            
+        def print_on_board(fit_q_table):
+            if self.verbose > 2:
+                print('-' * self.N * 11)
+                count = -1
+                for i in range(self.N):
+                    for j in range(self.N):
+                        count += 1
+                        if fit_q_table[i][j] == -1:
+                            print('%10s' % ' ', end='|')
+                        else:
+                            print('%10s' % (str(count) + "," + str(fit_q_table[i][j])), end='|')
+                    print()
+                print('PASS =', self.recent_state_q_values[-1])
+                print('-' * self.N * 11)
+            else:
+                print('-' * self.N * 6)
+                for i in range(self.N):
+                    for j in range(self.N):
+                        if fit_q_table[i][j] == -1:
+                            print('     ', end='|')
+                        else:
+                            print('%5s' % str(fit_q_table[i][j]), end='|')
+                    print()
+                print('PASS =', self.recent_state_q_values[-1])
+                print('-' * self.N * 6)
+            
+        self._ensure_recent_state(board_state)
+        if encoding_inc is not None and self.index_of_encoding not in encoding_inc:
+            return
+        
+        if self.verbose > 3:
+            print('encoding is %d' % self.index_of_encoding)
+            print('before transformation')
+            print(self.recent_state_q_values)
+            print()
+            print_on_board(deepcopy(self.recent_state_q_values)[:25].reshape((self.N, self.N)))
+        
+        fit_q_table = fit_table(self.recent_state_q_values).reshape((self.N, self.N))
+        print('after transformation')
+        print_on_board(fit_q_table)
+        
     
     def _state_q_values(self, board_state):      
         def encode(arr2d):
@@ -76,6 +142,7 @@ class QValues:
                 for i in range(len(arr)):
                     encoding += str(arr[mapping[i]])
                 encodings.append(encoding)
+            
             costs = [int(encoding) for encoding in encodings]
             index_of_encoding = np.argmin(costs)
             return index_of_encoding, encodings[index_of_encoding]
@@ -87,24 +154,25 @@ class QValues:
             relevant_q_values = np.ones(26)*self.initial_reward
             self.q_tables[board_state_encoded] = relevant_q_values
     
-        return index_of_encoding, np.asarray(relevant_q_values)
+        return index_of_encoding, board_state_encoded, np.asarray(relevant_q_values), 
     
     def _ensure_recent_state(self, board_state):
-        if board_state != self.recent_board_state:         
-            self.recent_board_state = board_state
-            self.index_of_encoding, self.recent_state_q_values =\
+        if board_state != self.recent_board_state:
+            self.recent_board_state = deepcopy(board_state)
+            self.index_of_encoding, self.recent_board_encoding, self.recent_state_q_values =\
                 self._state_q_values(board_state)
     
     def _to_fit_listing(self, action):        
         listing = self.action_to_listing[action]
         if listing != 25:
-            self.sym_dict_backward[self.index_of_encoding][listing]
+            # todo check this again
+            return self.sym_dict_forward[self.index_of_encoding][listing]
         return listing
     
     def _to_fit_action(self, listing):
         action = self.listing_to_action[listing]
         return self.listing_to_action[self._to_fit_listing(action)]
-        
+
     def __getitem__(self, selector):
         board_state, action = selector
         self._ensure_recent_state(board_state)
@@ -198,14 +266,15 @@ class QValues:
         
 class QLearner:
     
-    def __init__(self, N=5, alpha=0.5, gamma=0.5, initial_reward=-0.5):
+    def __init__(self, N=5, alpha=0.5, gamma=0.5, initial_reward=-0.5,
+                 training=False):
         self.N = 5
         self.type = 'qlearner'
         self.alpha = 0.5
         self.gamma = 0.5
         self.q_values = QValues(N, initial_reward)
         self.play_log = []
-        self.training = False
+        self.training = training
     
     def get_input(self, state, piece_type, epsilon=0.1):
 
@@ -220,6 +289,9 @@ class QLearner:
                         test_check = True
                         ):
                     self.q_values[(state.board, (i, j))] = -1
+                # else:
+                    # print((i, j), end="  ")
+        # print()
         
         # TODO Exploration Vs Exploitation
         # Follow epsilon greedy policy
@@ -232,6 +304,8 @@ class QLearner:
         # keep track of movements to learn later from them
         self.play_log.append([deepcopy(state.board), action])
         
+        # self.q_values.visualize_q_table(state.board)
+
         return action
     
     def train(self, go, epochs, reset=False, save_freq=100, ckpt='q_values', verbose=0):
@@ -240,6 +314,7 @@ class QLearner:
         import os
         
         go.verbose = True if verbose == 2 else False
+        self.q_values.verbose=verbose
             
         if not reset and os.path.isfile(ckpt + '.json'):
             self.q_values.load(filename=ckpt)
@@ -274,7 +349,7 @@ class QLearner:
     def learn(self, winner, verbose):
         if verbose:
             print('Play-log consists of %d states' % len(self.play_log))
-            print('Q-values consists of %d tables' % len(self.q_values))
+            print('Q-values consists of %d tables' % len(self.q_values.q_tables))
 
         board_state, action = self.play_log.pop()
         if verbose:
@@ -303,7 +378,6 @@ class QLearner:
             self.q_values[(board_state, action)] =\
                 (1-self.alpha)*(current_q) + self.alpha*(self.gamma*max_q)
             max_q = self.q_values.max_q(board_state)
-            
 
 def visualize_board_encoding(board_encoding, n=5):
     col = 0
@@ -315,13 +389,12 @@ def visualize_board_encoding(board_encoding, n=5):
             print("\n----------")
             col = 0
         
-        
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('--play', '-p', type=bool, help='play given input file', default=False)
     parser.add_argument('--train', '-t', type=int, help='learn q-values', default=0)
-    parser.add_argument('--save_freq', '-s', type=int, help='freq of saving q_values', default=10)
+    parser.add_argument('--save_freq', '-s', type=int, help='freq of saving q_values', default=1)
     parser.add_argument('--ckpt', '-c', type=str, help='ckpt filename', default='q_values')
     parser.add_argument('--reset', '-r', type=bool, help='reset q-values', default=False)
     parser.add_argument('--verbose', '-v', type=int, help='print board', default=0)
@@ -332,7 +405,7 @@ if __name__ == '__main__':
     q_learner = QLearner()
     
     if args.train > 0:
-        q_learner.training = True
+        q_learner.training = False
         q_learner.train(go, 
                         epochs=args.train, reset=args.reset, 
                         save_freq=args.save_freq, ckpt=args.ckpt,
